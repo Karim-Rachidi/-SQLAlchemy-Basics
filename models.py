3784133f2df9edb9d080230bc4f9f7ac4c4f8195
Karim-Rachidi <krimirachidi1@gmail.com>
2025-07-16 17:20:29 +0200

    Add files via upload

---
+++ models.py
@@ -0,0 +1,43 @@
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///users.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+    nickname = Column(String)
+
+    def __repr__(self):
+        return f'<User(name={self.name}, fullname={self.fullname}, nickname={self.nickname})>'
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
+
+    karim_user = User(name='Karim', fullname='Karim Rachidi', nickname='krimi')
+    session.add(karim_user)
+    session.commit()
+
+    print(karim_user.name)
+    print(karim_user.id)
+    print(karim_user.id)
+  
+    new_users = [User(name='Grace', fullname='Grace Hopper', nickname='Pioneer'), 
+     User(name='Alan', fullname='Alan Turing', nickname='Computer Scientist'),  
+     User(name='Katherine', fullname='Katherine Johnson', nickname='') ]
+    
+    session.add_all(new_users)
+    session.commit()
+    print(User(name='Grace', fullname='Grace Hopper', nickname='Pioneer').id)
+    for user in new_users:
+        print(user.id)
+
+    print(User(name='Grace', fullname='Grace Hopper', nickname='Pioneer').id)
+
